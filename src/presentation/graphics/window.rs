be4151967d394ed5bd6a5ded3afa58c60cad8908
John So <jungwookso@gmail.com>
2019-01-23 19:43:11 -0800

    SDL context window

--- src/presentation/graphics/window.rs
+++ src/presentation/graphics/window.rs
@@ -1,36 +1,34 @@
-extern crate glfw;
-
-use glfw::{Action, Context, Key};
+//extern crate glfw;
+extern crate sdl2;
+//use glfw::{Action, Context, Key};
 
 pub fn create_window() {
-    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+    let _window = video_subsystem
+        .window("Contagion", 1000, 600)
+        .resizable()
+        .build()
+        .unwrap();
 
-    // Create window and OpenGL context
-    let (mut window, events) = glfw.create_window(1000, 800, "Contagion", glfw::WindowMode::Windowed)
-        .expect("Failed to create GLFW window!");
-
-    // Set window's context current
-    window.make_current();
-    window.set_key_polling(true);
-
-    // Loop until user closes window
-    while !window.should_close() {
-        window.swap_buffers();
-
-        // Poll and process events
-        glfw.poll_events();
-        for (_, event) in glfw::flush_messages(&events) {
-            println!("{:?}", event);
-            handle_window_event(&mut window, event);
+    let mut event_pump = sdl.event_pump().unwrap();
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit { .. } => break 'main,
+                _ => {}
+            }
         }
+
+        // render window contents here
     }
 }
 
-fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
-    match event {
-        glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-            window.set_should_close(true)
-        },
-        _ => {},
-    }
-}
+//fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
+//    match event {
+//        glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+//            window.set_should_close(true)
+//        },
+//        _ => {},
+//    }
+//}
