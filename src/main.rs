1add642da36bbdbe093a34b90ce2b85adce7b9e9
John So <jungwookso@gmail.com>
2019-01-26 23:36:51 -0800

    main loop inside main.rs refactor

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,53 @@ pub mod presentation;
 pub mod simulation;
 
 #[macro_use] extern crate glium;
+extern crate glium_sdl2;
+extern crate sdl2;
+extern crate image;
+
+use std::io::Cursor;
+use glium::Surface;
+use glium::texture;
+
+fn init() -> Result<(glium_sdl2::SDL2Facade, sdl2::EventPump), String> {
+    // TODO: initialize music
+
+    // initialize window and eventpump
+    let window_tuple = presentation::graphics::renderer::create_window();
+    let mut window = window_tuple.0;
+    let mut event_pump = window_tuple.1;
+
+    Ok((window, event_pump))
+}
 
 fn main() {
-    presentation::graphics::renderer::create_window();
+    // init
+    let (mut window, mut event_pump) = match init() {
+        // error handler if init fails
+        Ok(t) => t,
+        Err(err) => {
+            println!("{}",err);
+            std::process::exit(1);
+        },
+    };
+
+    // main game loop
+    let mut running = true;
+    while running {
+        // draw background
+        let mut target = window.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.finish().unwrap();
+
+        for event in event_pump.poll_iter() {
+            use sdl2::event::Event;
+            match event {
+                // TODO: key inputs
+                Event::Quit { .. } => {
+                    running = false;
+                },
+                _ => ()
+            }
+        }
+    }
 }
--- src/presentation/graphics/renderer.rs
+++ src/presentation/graphics/renderer.rs
@@ -18,109 +18,78 @@ use sdl2::keyboard::Keycode;
 const WIDTH: u32 = 1024;
 const HEIGHT: u32 = 768;
 
-pub fn create_window() {
-    use glium_sdl2::DisplayBuild;
-    // initialize SDL library
-    let sdl_context = sdl2::init().unwrap();
-    // initialize video subsystem
-    let video_subsystem = sdl_context.video().unwrap();
-    // OpenGL context getters and setters
-    let gl_attr = video_subsystem.gl_attr();
-    let mut pause_time = false;
-
-    // OpenGL version switcher for platform compatibility
-    let mut major = 0;
-    let mut minor = 0;
-    if cfg!(macos) {
-        major = 4;
-        minor = 1;
-    }
-    if cfg!(linux) {
-        major = 4;
-        minor = 1;
-    }
-    if cfg!(windows) {
-        major = 4;
-        minor = 5;
-    }
-
-    // setup OpenGL profile
-    gl_attr.set_context_profile(sdl2::video::GLProfile::Core); // setting type of GL context
-    // Set the context into debug mode
-    gl_attr.set_context_flags().debug().set();
-    gl_attr.set_context_version(4, 5); // specifying OpenGL version
-
-    // creating window
-    // available functionality: https://nukep.github.io/rust-sdl2/sdl2/video/struct.WindowBuilder.html#method.resizable
-    let window = video_subsystem
-        .window("Contagion", WIDTH, HEIGHT)
-        .resizable()
-        .build_glium()
-        .unwrap();
-
-    // load texture for now
-    // TODO: probably need to refactor this elsewhere for loading other textures
-    let image = image::load(Cursor::new(&include_bytes!("../../assets/zombie-transparent.png")[..]),
-                            image::PNG).unwrap().to_rgba();
-    let image_dimensions = image.dimensions();
-    let image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
-    let texture = glium::texture::Texture2d::new(&window, image).unwrap();
 
 
-    #[derive(Copy, Clone)]
-    struct Vertex {
-        position: [f32; 2],
-        tex_coords: [f32; 2],
-    }
+pub fn create_window() -> (glium_sdl2::SDL2Facade, sdl2::EventPump) {
+  use glium_sdl2::DisplayBuild;
+  // initialize SDL library
+  let sdl_context = sdl2::init().unwrap();
+  // initialize video subsystem
+  let video_subsystem = sdl_context.video().unwrap();
+  // OpenGL context getters and setters
+  let gl_attr = video_subsystem.gl_attr();
+  let mut pause_time = false;
 
-    implement_vertex!(Vertex, position, tex_coords);
+  // OpenGL version switcher for platform compatibility
+  let mut major = 0;
+  let mut minor = 0;
+  if cfg!(macos) {
+      major = 4;
+      minor = 1;
+  }
+  if cfg!(linux) {
+      major = 4;
+      minor = 1;
+  }
+  if cfg!(windows) {
+      major = 4;
+      minor = 5;
+  }
 
-    let vertex1 = Vertex { position: [-0.5, -0.5], tex_coords: [0.0, 0.0] };
-    let vertex2 = Vertex { position: [ 0.0,  0.5], tex_coords: [0.0, 1.0] };
-    let vertex3 = Vertex { position: [ 0.5, -0.5], tex_coords: [1.0, 0.0] };
-    let shape = vec![vertex1, vertex2, vertex3];
+  // setup OpenGL profile
+  gl_attr.set_context_profile(sdl2::video::GLProfile::Core); // setting type of GL context
+  // Set the context into debug mode
+  gl_attr.set_context_flags().debug().set();
+  gl_attr.set_context_version(4, 5); // specifying OpenGL version
 
-    let vertex_buffer = glium::VertexBuffer::new(&window, &shape).unwrap();
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+  // creating window
+  // available functionality: https://nukep.github.io/rust-sdl2/sdl2/video/struct.WindowBuilder.html#method.resizable
+  let window = video_subsystem
+      .window("Contagion", WIDTH, HEIGHT)
+      .resizable()
+      .build_glium()
+      .unwrap();
+  let mut event_pump = sdl_context.event_pump().unwrap();
 
-    let mut time = Instant::now();
-
-    let program = glium::Program::from_source(&window, include_str!("vs.vert"), include_str!("fs.frag"), None).unwrap();
-
-    // main loop
-    let mut running = true;
-    let mut event_pump = sdl_context.event_pump().unwrap();
-    while running {
-        // calculate time
-        let elapsed_t = time.elapsed().as_secs() as f32;
-//        println!("{}", elapsed_t);
-
-        let mut target = window.draw();
-        // do drawing here...
-        target.clear_color(0.0, 0.0, 1.0, 1.0);
-        let uniforms = uniform! {
-            matrix: [
-                [1.0, 0.0, 0.0, 0.0],
-                [0.0, 1.0, 0.0, 0.0],
-                [0.0, 0.0, 1.0, 0.0],
-                [ elapsed_t/60.0 , 0.0, 0.0, 1.0f32],
-            ],
-            tex: &texture,
-        };
-        target.draw(&vertex_buffer, &indices, &program, &uniforms,
-                    &Default::default()).unwrap();
-        target.finish().unwrap();
-
-        // Event loop: polls for events sent to all windows
-        for event in event_pump.poll_iter() {
-            use sdl2::event::Event;
-            match event {
-                // TODO: key inputs
-                Event::Quit { .. } => {
-                    running = false;
-                },
-                _ => ()
-            }
-        }
-    }
+  (window, event_pump)
 }
+
+//pub fn load_textures(window: glium_sdl2::SDL2Facade) -> (glium::Program) {
+//    // load texture for now
+//    // TODO: probably need to refactor this elsewhere for loading other textures
+//    let image = image::load(Cursor::new(&include_bytes!("../../assets/zombie-transparent.png")[..]),
+//                            image::PNG).unwrap().to_rgba();
+//    let image_dimensions = image.dimensions();
+//    let image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
+//    let texture = glium::texture::Texture2d::new(&window, image).unwrap();
+//
+//
+//    #[derive(Copy, Clone)]
+//    struct Vertex {
+//        position: [f32; 2],
+//        tex_coords: [f32; 2],
+//    }
+//
+//    implement_vertex!(Vertex, position, tex_coords);
+//
+//    let vertex1 = Vertex { position: [-0.5, -0.5], tex_coords: [0.0, 0.0] };
+//    let vertex2 = Vertex { position: [ 0.0,  0.5], tex_coords: [0.0, 1.0] };
+//    let vertex3 = Vertex { position: [ 0.5, -0.5], tex_coords: [1.0, 0.0] };
+//    let shape = vec![vertex1, vertex2, vertex3];
+//
+//    let vertex_buffer = glium::VertexBuffer::new(&window, &shape).unwrap();
+//    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+//
+//    let program = glium::Program::from_source(&window, include_str!("vs.vert"), include_str!("fs.frag"), None).unwrap();
+//    (program, texture)
+//}
